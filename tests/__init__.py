76839de8acfd60fcf62d36b3c9e93215e36ad163
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2006-04-13 15:06:34 +0000

    `IWikiSyntaxProvider` related unit-tests are now dispatched in their relevant modules.
     * Better display of wiki unit-tests failure, so that it's easy
       to grasp the difference between ''expected'' result and 
       ''actual'' result.
     * In `trac.wiki.tests.formatter`, the link resolver syntax is tested
       using a sample link resolver
     * finally, there's a test file trac/tests/allwiki.py file which 
       can be used to test all the wiki related tests in Trac core.
    
    Now it becomes fairly easy for plugins to add their own tests
    for the wiki syntax they provide.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,13 @@
 import unittest
 
-from trac.wiki.tests import formatter, model
+from trac.wiki.tests import formatter, model, wikisyntax
 
 def suite():
 
     suite = unittest.TestSuite()
     suite.addTest(formatter.suite())
     suite.addTest(model.suite())
+    suite.addTest(wikisyntax.suite())
     return suite
 
 if __name__ == '__main__':
